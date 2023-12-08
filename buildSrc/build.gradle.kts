d3826f935983404de0bbaa6bede10e70365d212a
fstaudt <fstaudt@gmail.com>
2023-12-08 22:55:25 +0100

    build: gradle task to fetch input for day

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,10 +4,21 @@ repositories {
 
 plugins {
     kotlin("jvm") version embeddedKotlinVersion
+    `java-gradle-plugin`
 }
 
 dependencies {
     implementation("org.apache.httpcomponents:httpclient:4.5.14") {
         exclude("commons-codec", "commons-codec")
+    }
+}
+
+
+gradlePlugin {
+    plugins {
+        create("aoc") {
+            id = "buildSrc.aoc"
+            implementationClass = "com.github.fstaudt.aoc.AdventOfCodePlugin"
+        }
     }
 }
