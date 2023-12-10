92bdcef8ade3b548e59b93162f5ba549bd30da75
fstaudt <fstaudt@gmail.com>
2023-12-10 14:37:09 +0100

    Day 10: Pipe Maze

---
+++ src/test/kotlin/com/github/fstaudt/aoc2023/day10/Day10Test.kt
@@ -0,0 +1,28 @@
+package com.github.fstaudt.aoc2023.day10
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day10Test {
+
+    @Test
+    fun `part 1 should produce expected result for example`() {
+        assertThat(Day10("example_day10_part1.txt").part1()).isEqualTo(8)
+    }
+
+    @Test
+    fun `part 2 should produce expected result for example`() {
+        assertThat(Day10("example_day10_part2.txt").part2()).isEqualTo(4)
+        assertThat(Day10("example_day10_part2_2.txt").part2()).isEqualTo(4)
+    }
+
+    @Test
+    fun `part 1 should produce expected result for my input`() {
+        assertThat(Day10().part1()).isEqualTo(7030)
+    }
+
+    @Test
+    fun `part 2 should produce expected result for my input`() {
+        assertThat(Day10().part2()).isEqualTo(285)
+    }
+}
