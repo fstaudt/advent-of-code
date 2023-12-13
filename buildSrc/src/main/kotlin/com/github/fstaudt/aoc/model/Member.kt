bb4b75db5647c7473e1ed8d1713e3650b804ecaa
fstaudt <fstaudt@gmail.com>
2023-12-13 17:55:20 +0100

    build: ranking until EOD in leaderboard slope chart

--- buildSrc/src/main/kotlin/com/github/fstaudt/aoc/model/Member.kt
+++ buildSrc/src/main/kotlin/com/github/fstaudt/aoc/model/Member.kt
@@ -11,6 +11,7 @@ data class Member(
     @JsonProperty("local_score") val localScore: Int,
     @JsonProperty("global_score") val globalScore: Int,
     @JsonProperty("completion_day_level") val completionDayLevels: CompletionDayLevels,
+    @JsonIgnore var localDailyScore: Int = 0,
     @JsonIgnore val localDailyScores: MutableList<Int> = mutableListOf(),
     @JsonIgnore val rankings: MutableList<Int?> = mutableListOf(),
 ) {
--- buildSrc/src/main/kotlin/com/github/fstaudt/aoc/service/LeaderboardService.kt
+++ buildSrc/src/main/kotlin/com/github/fstaudt/aoc/service/LeaderboardService.kt
@@ -5,41 +5,55 @@ import com.github.fstaudt.aoc.model.CompletionPartsFunctions.part1
 import com.github.fstaudt.aoc.model.CompletionPartsFunctions.part2
 import com.github.fstaudt.aoc.model.Leaderboard
 import com.github.fstaudt.aoc.model.Member
+import kotlin.Int.Companion.MAX_VALUE
 
 class LeaderboardService {
 
-    internal fun topMembers(leaderboard: Leaderboard, top: Int): List<Member> {
+    fun topMembers(leaderboard: Leaderboard, top: Int): List<Member> {
+        leaderboard.computeLocalDailyScores()
         for (day in 0..<leaderboard.numberOfDays()) {
-            leaderboard.sortedBy { it.completionDayLevels.day(day)?.part1() }.let { members ->
-                members.forEachIndexed { index, member ->
-                    val previousDailyScore = if (day > 0) member.localDailyScores[day - 1] else 0
-                    if (member.completionDayLevels.day(day)?.part1() != null) {
-                        previousDailyScore + members.size - index
-                    } else {
-                        previousDailyScore
-                    }.let { member.localDailyScores.add(it) }
-                }
-            }
-            leaderboard.sortedBy { it.completionDayLevels.day(day)?.part2() }.let { members ->
-                members.forEachIndexed { index, member ->
-                    if (member.completionDayLevels.day(day)?.part2() != null) {
-                        member.localDailyScores[day] += members.size - index
-                    }
-                }
-            }
             leaderboard.members.values.sortedWith(
                 compareByDescending<Member> { it.localDailyScores[day] }
-                    .thenBy { it.completionDayLevels.day(day)?.part2()?.starIndex ?: Int.MAX_VALUE }
-                    .thenBy { it.completionDayLevels.day(day)?.part1()?.starIndex ?: Int.MAX_VALUE }
+                    .thenBy { it.completionDayLevels.day(day)?.part2()?.starIndex ?: MAX_VALUE }
+                    .thenBy { it.completionDayLevels.day(day)?.part1()?.starIndex ?: MAX_VALUE }
             ).let { members ->
                 members.forEachIndexed { index, member ->
                     member.rankings.add((index + 1).takeIf { it <= top })
                 }
             }
         }
-
         return leaderboard.members.values
             .sortedWith(compareByDescending<Member> { it.localScore }.thenBy { it.lastStarTimestamp })
             .filter { member -> member.rankings.any { it != null } }
+    }
+
+    private fun Leaderboard.computeLocalDailyScores() {
+        members.values.forEach { it.localDailyScores.clear() }
+        for (limitDay in 0..<numberOfDays()) {
+            val start = java.time.LocalDate.of(event.toInt(), java.time.Month.DECEMBER, 1).let {
+                java.time.OffsetDateTime.of(it, java.time.LocalTime.MIN, java.time.ZoneOffset.UTC).toEpochSecond()
+            }
+            val limit = (start + (limitDay + 1) * 86400)
+            for (day in 0..limitDay) {
+                sortedBy { it.completionDayLevels.day(day)?.part1()?.ifBefore(limit) }.let { members ->
+                    members.forEachIndexed { index, member ->
+                        if (member.completionDayLevels.day(day)?.part1()?.ifBefore(limit) != null) {
+                            member.localDailyScore += members.size - index
+                        }
+                    }
+                }
+                sortedBy { it.completionDayLevels.day(day)?.part2()?.ifBefore(limit) }.let { members ->
+                    members.forEachIndexed { index, member ->
+                        if (member.completionDayLevels.day(day)?.part2()?.ifBefore(limit) != null) {
+                            member.localDailyScore += members.size - index
+                        }
+                    }
+                }
+            }
+            members.values.forEach {
+                it.localDailyScores.add(it.localDailyScore)
+                it.localDailyScore = 0
+            }
+        }
     }
 }
