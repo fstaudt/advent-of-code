31541fa0d03073e64d17df98ee688a60a882cf36
fstaudt <fstaudt@gmail.com>
2023-12-16 08:03:09 +0100

    Day 12: Hot Springs - part2

--- aoc2023/src/main/kotlin/com/github/fstaudt/aoc2023/day12/Day12.kt
+++ aoc2023/src/main/kotlin/com/github/fstaudt/aoc2023/day12/Day12.kt
@@ -1,9 +1,8 @@
 package com.github.fstaudt.aoc2023.day12
 
 import com.github.fstaudt.aoc.shared.Day
-import com.github.fstaudt.aoc.shared.StringExtensions.splitInts
-import com.github.fstaudt.aoc.shared.StringExtensions.splitNotEmpty
 import com.github.fstaudt.aoc.shared.Input.readInputLines
+import com.github.fstaudt.aoc.shared.StringExtensions.splitInts
 
 fun main() {
     Day12().run()
@@ -13,38 +12,74 @@ class Day12(fileName: String = "day_12.txt") : Day {
     override val input: List<String> = readInputLines(fileName)
     private val field = input.map { it.toRow() }
 
-    data class Row(val springs: String, val numbers: List<Int>)
-
     private fun String.toRow() = Row(split(" ")[0], split(" ")[1].splitInts(','))
     override fun part1() = sumCountOfArrangements()
 
     override fun part2() = sumCountOfArrangementsWhenUnfolded()
 
     private fun sumCountOfArrangements(): Long {
-        return field.sumOf { it.toBruteForceArrangements() }
-    }
-
-    private fun Row.toBruteForceArrangements(): Long {
-        if (springs.none { it == '?' }) {
-            return if (springs.splitNotEmpty('.').map { it.length } == numbers) 1 else 0
-        }
-        return Row(springs.replaceFirst('?', '.'), numbers).toBruteForceArrangements() +
-                Row(springs.replaceFirst('?', '#'), numbers).toBruteForceArrangements()
+        return field.sumOf { it.toArrangements() }
     }
 
     private fun sumCountOfArrangementsWhenUnfolded(): Long {
         return field
-            .map { row -> Row(row.springs.let { "$it?".repeat(4) + it }, (1..5).flatMap { row.numbers }) }
+            .map { row -> Row((1..5).map { row.springs }.joinToString("?") { it }, (1..5).flatMap { row.numbers }) }
             .map { it.toArrangements() }
             .sumOf { it }
     }
 
-    private fun Row.toArrangements(): Long {
-        // TODO
-        return numbers.toRegex().pattern.length.toLong()
-    }
+    data class Row(val springs: String, val numbers: List<Int> = emptyList()) {
+        companion object {
+            private val countCache = mutableMapOf<String, Long>()
+            private val countAfterDamagedCache = mutableMapOf<String, Long>()
+        }
 
-    private fun List<Int>.toRegex(): Regex {
-        return joinToString("(\\.*[\\.?]\\.*)", "^[\\.?]*", "[\\.?]*$") { "([?]*[#?]{$it}[?]*)" }.toRegex()
+        fun toArrangements(): Long {
+            val masks = ".${numbers.joinToString(".") { "#".repeat(it) }}."
+            return count(springs, masks)
+        }
+
+        private fun count(springs: String, masks: String): Long {
+            val cacheKey = "$masks+$springs"
+            return countCache[cacheKey] ?: basicCount(springs, masks) ?: when (springs.first()) {
+                '.' ->
+                    if (masks.first() == '.') count(springs.drop(1), masks)
+                    else 0
+
+                '#' ->
+                    if (masks.first() == '.') countAfterDamaged(springs, masks.drop(1))
+                    else countAfterDamaged(springs.drop(1), masks.drop(1))
+
+                else -> // '?'
+                    if (masks.first() == '.') count(springs.drop(1), masks) + countAfterDamaged(springs, masks.drop(1))
+                    else countAfterDamaged(springs.drop(1), masks.drop(1))
+            }.also { countCache[cacheKey] = it }
+        }
+
+        private fun countAfterDamaged(springs: String, masks: String): Long {
+            val cacheKey = "$masks+$springs"
+            return countAfterDamagedCache[cacheKey] ?: basicCount(springs, masks) ?: when (springs.first()) {
+                '.' ->
+                    if (masks.first() == '.') count(springs.drop(1), masks)
+                    else 0
+
+                '#' ->
+                    if (masks.first() == '.') 0
+                    else countAfterDamaged(springs.drop(1), masks.drop(1))
+
+                else -> // '?'
+                    if (masks.first() == '.') count(springs.drop(1), masks)
+                    else countAfterDamaged(springs.drop(1), masks.drop(1))
+            }.also { countAfterDamagedCache[cacheKey] = it }
+        }
+
+        private fun basicCount(springs: String, masks: String): Long? {
+            return when {
+                masks == "." && springs.all { it == '.' || it == '?' } -> 1
+                masks == "." -> 0
+                springs.isEmpty() -> 0
+                else -> null
+            }
+        }
     }
 }
--- aoc2023/src/test/kotlin/com/github/fstaudt/aoc2023/day12/Day12Test.kt
+++ aoc2023/src/test/kotlin/com/github/fstaudt/aoc2023/day12/Day12Test.kt
@@ -1,8 +1,11 @@
 package com.github.fstaudt.aoc2023.day12
 
+import com.github.fstaudt.aoc2023.day12.Day12.Row
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
 
 class Day12Test {
     @Test
@@ -16,14 +19,40 @@ class Day12Test {
     }
 
     @Test
-    @Disabled("doesn't work")
     fun `part 2 should produce expected result for example`() {
         assertThat(Day12("example_day12.txt").part2()).isEqualTo(525152)
     }
 
     @Test
-    @Disabled("doesn't work")
     fun `part 2 should produce expected result for my input`() {
-        assertThat(Day12().part2()).isEqualTo(0)
+        assertThat(Day12().part2()).isEqualTo(45322533163795)
+    }
+
+    companion object {
+        @JvmStatic
+        fun arrangementsTestData() = listOf(
+            of("", listOf(1), 0),
+            of(".", listOf(1), 0),
+            of("?", listOf(1), 1),
+            of("#", listOf(1), 1),
+            of(".?", listOf(1), 1),
+            of("?.", listOf(1), 1),
+            of(".#", listOf(1), 1),
+            of("#.", listOf(1), 1),
+            of("#?", listOf(1), 1),
+            of("??", listOf(1), 2),
+            of("???.###", listOf(1,1,3), 1),
+            of(".??..??...?##.", listOf(1,1,3), 4),
+            of("?#?#?#?#?#?#?#?", listOf(1,3,1,6), 1),
+            of("????.#...#...", listOf(4,1,1), 1),
+            of("????.######..#####.", listOf(1,6,5), 4),
+            of("?###????????", listOf(3,2,1), 10),
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrangementsTestData")
+    fun `toArrangements should produce expected results`(springs: String, numbers: List<Int>, result: Long) {
+        assertThat(Row(springs, numbers).toArrangements()).isEqualTo(result)
     }
 }
