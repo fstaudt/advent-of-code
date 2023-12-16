1f419047e90132682f2434c699622d8b63854ed8
fstaudt <fstaudt@gmail.com>
2023-12-16 08:03:09 +0100

    Day 12: Hot Springs - part2

--- aoc2023/build.gradle.kts
+++ aoc2023/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
 }
 
 tasks.test {
