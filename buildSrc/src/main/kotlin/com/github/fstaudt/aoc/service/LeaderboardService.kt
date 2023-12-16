5b91f6ecef481cd359f782bd0158da2bf2c58ab6
fstaudt <fstaudt@gmail.com>
2023-12-16 08:56:28 +0100

    build: options for task leaderboardSlopeChart to limit days displayed in chart

--- buildSrc/src/main/kotlin/com/github/fstaudt/aoc/service/LeaderboardService.kt
+++ buildSrc/src/main/kotlin/com/github/fstaudt/aoc/service/LeaderboardService.kt
@@ -9,9 +9,9 @@ import kotlin.Int.Companion.MAX_VALUE
 
 class LeaderboardService {
 
-    fun topMembers(leaderboard: Leaderboard, top: Int): List<Member> {
-        leaderboard.computeLocalDailyScores()
-        for (day in 0..<leaderboard.numberOfDays()) {
+    fun topMembers(leaderboard: Leaderboard, top: Int, until: Int = leaderboard.numberOfDays()): List<Member> {
+        leaderboard.computeLocalDailyScores(until)
+        for (day in 0..<until) {
             leaderboard.members.values.sortedWith(
                 compareByDescending<Member> { it.localDailyScores[day] }
                     .thenBy { it.completionDayLevels.day(day)?.part2()?.starIndex ?: MAX_VALUE }
@@ -23,13 +23,13 @@ class LeaderboardService {
             }
         }
         return leaderboard.members.values
-            .sortedWith(compareByDescending<Member> { it.localScore }.thenBy { it.lastStarTimestamp })
+            .sortedWith(compareByDescending<Member> { it.localDailyScores[until - 1] }.thenBy { it.lastStarTimestamp })
             .filter { member -> member.rankings.any { it != null } }
     }
 
-    private fun Leaderboard.computeLocalDailyScores() {
+    private fun Leaderboard.computeLocalDailyScores(until: Int) {
         members.values.forEach { it.localDailyScores.clear() }
-        for (limitDay in 0..<numberOfDays()) {
+        for (limitDay in 0..<until) {
             val start = java.time.LocalDate.of(event.toInt(), java.time.Month.DECEMBER, 1).let {
                 java.time.OffsetDateTime.of(it, java.time.LocalTime.MIN, java.time.ZoneOffset.UTC).toEpochSecond()
             }
