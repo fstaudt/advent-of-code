3eacf2e2e6493d6c1f927772f3b8feae43f1a995
fstaudt <fstaudt@gmail.com>
2023-12-24 10:46:07 +0100

    Day 24: Never Tell Me The Odds

--- shared/src/main/kotlin/com/github/fstaudt/aoc/shared/StringExtensions.kt
+++ shared/src/main/kotlin/com/github/fstaudt/aoc/shared/StringExtensions.kt
@@ -2,8 +2,8 @@ package com.github.fstaudt.aoc.shared
 
 object StringExtensions {
     fun String.splitNotEmpty(delimiter: Char = ' '): List<String> = split(delimiter).filter { it.isNotEmpty() }
-    fun String.splitLongs(delimiter: Char = ' '): List<Long> = splitNotEmpty(delimiter).map { it.toLong() }
-    fun String.splitInts(delimiter: Char = ' '): List<Int> = splitNotEmpty(delimiter).map { it.toInt() }
+    fun String.splitLongs(delimiter: Char = ' '): List<Long> = splitNotEmpty(delimiter).map { it.trim().toLong() }
+    fun String.splitInts(delimiter: Char = ' '): List<Int> = splitNotEmpty(delimiter).map { it.trim().toInt() }
     fun String.toGroupValue(pattern: String, i: Int): String = pattern.toRegex().find(this)!!.groupValues[i]
     fun String.toGroupValues(pattern: String): List<String> = pattern.toRegex().find(this)!!.groupValues
     fun List<String>.flip() = get(0).mapIndexed { index, _ -> joinToString("") { "${it[index]}" } }
