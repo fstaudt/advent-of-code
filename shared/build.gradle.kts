1e51235c42fa81558ac1b5eef2f20b2bd183d6a3
fstaudt <fstaudt@gmail.com>
2024-11-06 13:29:49 +0100

    build: bump assertj to 3.26.3

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 val junitVersion: String by project
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
