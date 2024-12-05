0fad35e8d5d323c544bb13df01f568c69a044087
fstaudt <fstaudt@gmail.com>
2024-12-05 23:38:28 +0100

    build: handle new field day1_ts in private leaderboards API

--- buildSrc/src/main/kotlin/com/github/fstaudt/aoc/model/Leaderboard.kt
+++ buildSrc/src/main/kotlin/com/github/fstaudt/aoc/model/Leaderboard.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Leaderboard(
     val event: String,
+    @JsonProperty("day1_ts") val day1Timestamp: String?,
     @JsonProperty("owner_id") val ownerId: String,
     val members: Map<String, Member>,
 ) {
