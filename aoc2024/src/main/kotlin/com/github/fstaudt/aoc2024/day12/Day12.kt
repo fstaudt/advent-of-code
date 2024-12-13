de5ca69e9fdecab7bd2d3d209a175f09062404bb
fstaudt <fstaudt@gmail.com>
2024-12-13 10:12:37 +0100

    Day 12 2024: Garden Groups - part2

--- aoc2024/src/main/kotlin/com/github/fstaudt/aoc2024/day12/Day12.kt
+++ aoc2024/src/main/kotlin/com/github/fstaudt/aoc2024/day12/Day12.kt
@@ -4,6 +4,7 @@ import com.github.fstaudt.aoc.shared.Day
 import com.github.fstaudt.aoc.shared.Input.readInputLines
 import com.github.fstaudt.aoc.shared.Matrix
 import com.github.fstaudt.aoc.shared.MatrixExtensions.toMatrixOf
+import com.github.fstaudt.aoc2024.day12.Day12.Direction.*
 
 fun main() {
     Day12().run()
@@ -15,7 +16,7 @@ class Day12(fileName: String = "day_12.txt") : Day {
 
     override fun part1() = garden.regions.sumOf { it.area() * it.perimeter() }
 
-    override fun part2() = 0L
+    override fun part2() = garden.regions.sumOf { it.area() * it.sides() }
 
     data class Garden(val plots: Matrix<Plot>, val regions: MutableList<Region> = mutableListOf()) {
         fun plot(i: Int, j: Int) = plots.getOrNull(i)?.getOrNull(j)
@@ -54,6 +55,68 @@ class Day12(fileName: String = "day_12.txt") : Day {
             Direction.entries.sumOf { dir ->
                 if (plot(plot.i + dir.di, plot.j + dir.dj) == null) 1L else 0
             }
+        }
+
+        fun sides(): Long {
+            var sides = 0L
+            plots.map { it.i }.distinct().forEach { line ->
+                sides += hSides(line, UP)
+                sides += hSides(line, DOWN)
+            }
+            plots.map { it.j }.distinct().forEach { line ->
+                sides += vSides(line, LEFT)
+                sides += vSides(line, RIGHT)
+            }
+
+            return sides
+        }
+
+        private fun vSides(column: Int, leftRight: Direction): Long {
+            var sides = 0L
+            var side = false
+            plots.filter { it.j == column }.sortedBy { it.i }.forEach { p ->
+                if ((plot(p.i + leftRight.di, p.j + leftRight.dj) == null &&
+                        plot(p.i + UP.di, p.j + UP.dj) == null) ||
+                    (plot(p.i + leftRight.di, p.j + leftRight.dj) == null &&
+                        plot(p.i + leftRight.di + UP.di, p.j + leftRight.dj + UP.dj) != null)
+                ) {
+                    side = true
+                }
+                if (side == true &&
+                    (plot(p.i + leftRight.di, p.j + leftRight.dj) == null &&
+                        plot(p.i + DOWN.di, p.j + DOWN.dj) == null) ||
+                    (plot(p.i + leftRight.di, p.j + leftRight.dj) == null &&
+                        plot(p.i + leftRight.di + DOWN.di, p.j + leftRight.dj + DOWN.dj) != null)
+                ) {
+                    sides++
+                    side = false
+                }
+            }
+            return sides
+        }
+
+        private fun hSides(line: Int, upDown: Direction): Long {
+            var sides = 0L
+            var side = false
+            plots.filter { it.i == line }.sortedBy { it.j }.forEach { p ->
+                if ((plot(p.i + upDown.di, p.j + upDown.dj) == null &&
+                        plot(p.i + LEFT.di, p.j + LEFT.dj) == null) ||
+                    (plot(p.i + upDown.di, p.j + upDown.dj) == null &&
+                        plot(p.i + upDown.di + LEFT.di, p.j + upDown.dj + LEFT.dj) != null)
+                ) {
+                    side = true
+                }
+                if (side == true &&
+                    (plot(p.i + upDown.di, p.j + upDown.dj) == null &&
+                        plot(p.i + RIGHT.di, p.j + RIGHT.dj) == null) ||
+                    (plot(p.i + upDown.di, p.j + upDown.dj) == null &&
+                        plot(p.i + upDown.di + RIGHT.di, p.j + upDown.dj + RIGHT.dj) != null)
+                ) {
+                    sides++
+                    side = false
+                }
+            }
+            return sides
         }
     }
 
--- aoc2024/src/test/kotlin/com/github/fstaudt/aoc2024/day12/Day12Test.kt
+++ aoc2024/src/test/kotlin/com/github/fstaudt/aoc2024/day12/Day12Test.kt
@@ -2,6 +2,8 @@ package com.github.fstaudt.aoc2024.day12
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class Day12Test {
     @Test
@@ -14,13 +16,19 @@ class Day12Test {
         assertThat(Day12().part1()).isEqualTo(1456082)
     }
 
-    @Test
-    fun `part 2 should produce expected result for example`() {
-        assertThat(Day12("example_day12.txt").part2()).isEqualTo(0)
+    @ParameterizedTest
+    @CsvSource(
+        "example_day12.txt, 1206",
+        "example_day12_2.txt, 80",
+        "example_day12_3.txt, 236",
+        "example_day12_4.txt, 368"
+    )
+    fun `part 2 should produce expected result for example`(file: String, result: Long) {
+        assertThat(Day12(file).part2()).isEqualTo(result)
     }
 
     @Test
     fun `part 2 should produce expected result for my input`() {
-        assertThat(Day12().part2()).isEqualTo(0)
+        assertThat(Day12().part2()).isEqualTo(872382)
     }
 }
