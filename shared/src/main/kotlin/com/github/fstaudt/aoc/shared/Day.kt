3fed69ce7f67fd428a89f6de26c04f389d7a7218
fstaudt <fstaudt@gmail.com>
2024-12-24 08:55:34 +0100

    feat: display durations in Day.run

--- shared/src/main/kotlin/com/github/fstaudt/aoc/shared/Day.kt
+++ shared/src/main/kotlin/com/github/fstaudt/aoc/shared/Day.kt
@@ -1,5 +1,8 @@
 package com.github.fstaudt.aoc.shared
 
+import java.time.Duration.between
+import java.time.LocalDateTime.now
+
 interface Day {
 
     val input: List<Any>
@@ -9,7 +12,10 @@ interface Day {
     fun part2(): Long
 
     fun run() {
+        val start = now()
         println("Part 1: ${part1()}")
+        println("Duration: ${between(start, now()).toMillis()}ms")
         println("Part 2: ${part2()}")
+        println("Duration: ${between(start, now()).toMillis()}ms")
     }
 }
