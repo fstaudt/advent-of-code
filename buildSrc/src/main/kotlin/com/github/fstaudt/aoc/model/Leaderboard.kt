cfa7511b069670e11de5f31ed5c296f96b77a784
fstaudt <fstaudt@gmail.com>
2025-11-30 21:46:31 +0100

    build: bump Gradle to 9.1.0

--- buildSrc/src/main/kotlin/com/github/fstaudt/aoc/model/Leaderboard.kt
+++ buildSrc/src/main/kotlin/com/github/fstaudt/aoc/model/Leaderboard.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Leaderboard(
     val event: String,
-    @JsonProperty("day1_ts") val day1Timestamp: String?,
-    @JsonProperty("owner_id") val ownerId: String,
+    @param:JsonProperty("day1_ts") val day1Timestamp: String?,
+    @param:JsonProperty("owner_id") val ownerId: String,
     val members: Map<String, Member>,
 ) {
     fun owner() = members[ownerId]?.name() ?: "(anonymous user #$ownerId)"
