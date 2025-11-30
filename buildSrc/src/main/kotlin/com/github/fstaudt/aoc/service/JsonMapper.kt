2645bd7450e4d2f042af1485e7aea92750378d8d
fstaudt <fstaudt@gmail.com>
2025-11-30 22:19:53 +0100

    build: bump Jackson to 3.0.3

--- buildSrc/src/main/kotlin/com/github/fstaudt/aoc/service/JsonMapper.kt
+++ buildSrc/src/main/kotlin/com/github/fstaudt/aoc/service/JsonMapper.kt
@@ -1,17 +1,17 @@
 package com.github.fstaudt.aoc.service
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters.None
+import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.KotlinModule
 import java.io.File
 
 abstract class JsonMapper : BuildService<None> {
-    private val objectMapper = ObjectMapper().also {
-        it.registerModule(KotlinModule.Builder().build())
-    }
+  private val objectMapper = JsonMapper.builder()
+    .addModule(KotlinModule.Builder().build())
+    .build()
 
-    fun <T> readValue(file: File, valueType: Class<T>): T {
-        return objectMapper.readValue(file, valueType)
-    }
+  fun <T> readValue(file: File, valueType: Class<T>): T {
+    return objectMapper.readValue(file, valueType)
+  }
 }
